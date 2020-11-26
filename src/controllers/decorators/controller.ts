f200ed164b1403e677581f22adfefaef311d1b53
Darren Matis <darren.matis@sap.com>
2020-11-26 15:51:24 -0800

    DRY'd up the code with Enum

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
+import { Methods } from './Methods';
+import { MetadataKeys } from './MetadataKeys';
 
 export function controller(routePrefix: string) {
   return function(target: Function) {
@@ -9,10 +11,11 @@ export function controller(routePrefix: string) {
     // if so, extract the path (route) property and associate it with a router
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
-      const path = Reflect.getMetadata('path', target.prototype, key);
+      const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
+      const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler)
+        router[method](`${routePrefix}${path}`, routeHandler)
       }
     }
   }
